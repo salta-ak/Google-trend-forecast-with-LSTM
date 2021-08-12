8535526b819bf9058298ba40023fa9c84404cac5
Salta <75034184+salta-ak@users.noreply.github.com>
2021-08-12 21:31:04 +0600

    Update Google_Trend_load_data_plot.py

--- Google_Trend_load_data_plot.py
+++ Google_Trend_load_data_plot.py
@@ -8,19 +8,20 @@ pytrends = TrendReq(hl='en-US', tz=360)
 # Load data from with pytrend API
 
 kw_all=[kw_list,btc_list,st_list,cov_list]
+filename=['bitcoin','covid','stock_price']
 kw_list = ["bitcoin","COVID-19","stock price"]
 btc_list=["bitcoin","cryptocurrency","blockchain+bitcoin"]
 st_list=["stock+price","stock+index","futures+stock"]
 cov_list=["Covid","cough+covid","symptoms+covid"]
 count=0
 figure, axes = plt.subplots(4, 1, figsize=(15,15))
-for i in kw_all:
+for i,j in zip(kw_all,filename):
     pytrends.build_payload(i, cat=0, timeframe='today 5-y', geo='', gprop='')
     data=pytrends.interest_over_time()
     data=data.loc[~(data==0.0).all(axis=1)]
     series = data.iloc[: , :-1].astype(float).sort_index()
-    #series.to_csv('{}.csv'.format(i))
-    #figure, axes = plt.subplots(1, 1)
+    series.to_csv('{}.csv'.format(j))
+    figure, axes = plt.subplots(1, 1)
     series.plot(ax=axes[count])
     count+=1
     
