7be9acd0cb2f53ebd458195e0440a518a5b5e661
Salta <75034184+salta-ak@users.noreply.github.com>
2021-08-16 18:44:44 +0600

    Update script.py

--- experiment1_table2/script.py
+++ experiment1_table2/script.py
@@ -1,4 +1,92 @@
 
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras import layers
+from keras_tuner import RandomSearch
+from keras_tuner import BayesianOptimization
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM
+from keras.callbacks import CSVLogger
+
+
+
+df = pd.read_csv('bitcoin.csv', sep=',')
+df['date'] = pd.to_datetime(df['date'])
+df.set_index('date', inplace=True) 
+
+
+def create_dataset(data_series, look_back, split_frac, transforms):
+    
+    # log transforming that data, if necessary
+    
+    # differencing data, if necessary
+    if transforms[0] == True:
+        dates = data_series.index
+        data_series = pd.Series(data_series - data_series.shift(1), index=dates).dropna()
+
+    # scaling values between 0 and 1
+    dates = data_series.index
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data_series.values.reshape(-1, 1))
+    data_series = pd.Series(scaled_data[:, 0], index=dates)
+    
+    # creating targets and features by shifting values by 'i' number of time periods
+    df = pd.DataFrame()
+    for i in range(look_back+1):
+        label = ''.join(['t-', str(i)])
+        df[label] = data_series.shift(i)
+    df = df.dropna()
+    print(df.tail())
+    
+    # splitting data into train and test sets
+    size = int(split_frac*df.shape[0])
+    train = df[:size]
+    test = df[size:]
+    
+    # creating target and features for training set
+    X_train = train.iloc[:, 1:].values
+    y_train = train.iloc[:, 0].values
+    train_dates = train.index
+    
+    # creating target and features for test set
+    X_test = test.iloc[:, 1:].values
+    y_test = test.iloc[:, 0].values
+    test_dates = test.index
+    
+    # reshaping data into 3 dimensions for modeling with the LSTM neural net
+    X_train = np.reshape(X_train, (X_train.shape[0], 1, look_back))
+    X_test = np.reshape(X_test, (X_test.shape[0], 1, look_back))
+    
+    return X_train, y_train, X_test, y_test, train_dates, test_dates, scaler
+    
+    
+    
+def inverse_transforms(train_predict, y_train, test_predict, y_test, data_series, train_dates, test_dates, scaler, transforms):
+    
+    # inverse 0 to 1 scaling
+    train_predict = pd.Series(scaler.inverse_transform(train_predict.reshape(-1,1))[:,0], index=train_dates)
+    y_train = pd.Series(scaler.inverse_transform(y_train.reshape(-1, 1))[:,0], index=train_dates)
+
+    test_predict = pd.Series(scaler.inverse_transform(test_predict.reshape(-1, 1))[:,0], index=test_dates)
+    y_test = pd.Series(scaler.inverse_transform(y_test.reshape(-1, 1))[:,0], index=test_dates)
+    if transforms[0] == True:
+        train_predict = pd.Series(train_predict + data_series.shift(1), index=train_dates).dropna()
+        y_train = pd.Series(y_train + data_series.shift(1), index=train_dates).dropna()
+
+        test_predict = pd.Series(test_predict + data_series.shift(1), index=test_dates).dropna()
+        y_test = pd.Series(y_test + data_series.shift(1), index=test_dates).dropna()
+        
+    return train_predict, y_train, test_predict, y_test
+    
+    
+    #preparing our training and test dataset
+look_back = 12
+print("\n -------------------------------- \n Training Data \n -------------------------------- \n ")
+X_train, y_train, X_test, y_test, train_dates, test_dates, scaler = create_dataset(df.bitcoin, look_back, 0.8, [True])
 
 
   #building LSTM model 
@@ -15,7 +103,7 @@ modelckpt_callback = keras.callbacks.ModelCheckpoint( monitor="val_loss",filepat
 es_callback = CSVLogger('log.csv', append=True, separator=';')
 
 
-r = model.fit(X_train, y_train, epochs=100, batch_size=5, verbose=2,callbacks=[es_callback, modelckpt_callback,])
+r = model.fit(X_train, y_train, epochs=100, batch_size=1, verbose=2,callbacks=[es_callback, modelckpt_callback,])
 
 # making predictions
 train_predict = model.predict(X_train)
